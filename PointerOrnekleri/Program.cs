8ccfe3372bbab707591d09731811cf6a07717e4c
MEHMET <MEHMET@DESKTOP-H6NC22D>
2023-11-13 22:18:29 +0300

    Add project files.

---
+++ PointerOrnekleri/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointerOrnekleri
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                double a = 5;
+                double* ptr1;
+                ptr1 = &a;
+                *ptr1 = 10;
+                Console.WriteLine(a);
+                Console.WriteLine((long)ptr1);
+
+                char c = 'A';
+                char* ptr2;
+                ptr2 = &c;
+                *ptr2 = 'S';
+                Console.WriteLine(c);
+                Console.WriteLine((long)ptr2);
+                Console.WriteLine("-----------------------------");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ StackAlloc/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAlloc
+{
+    class Program
+    {
+       unsafe static void Main(string[] args)
+        {
+            Console.WriteLine("Dizi boyutu gir:");
+            int boyut = 0;
+            try
+            {
+                boyut = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            int* dizi= stackalloc int[boyut];
+            for (int i = 0;i<boyut;i++)
+            {
+                *(dizi + i) = i;
+                Console.WriteLine(dizi[i]);
+                Console.WriteLine((long)(dizi+i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
